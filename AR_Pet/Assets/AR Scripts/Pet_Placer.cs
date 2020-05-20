10bd3f45070231ec6b8aa732277c753867b0c513
EllayaZ <EllayaZ@users.noreply.github.com>
2020-05-20 12:58:57 -0700

    updates

--- AR_Pet/Assets/AR Scripts/Pet_Placer.cs
+++ AR_Pet/Assets/AR Scripts/Pet_Placer.cs
@@ -22,18 +22,69 @@ public class Pet_Placer : MonoBehaviour
         //selectionUI.SetActive(false);
     }
 
+    // 
+    void Update()
+    {
+        //check if we touching UI element or if we not touching we continue with "If" statment
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(Input.touches[0].fingerId))
+        {
+            //whereever we touch on teh screen
+            Ray ray = cam.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+
+                if (hit.collider.gameObject != null && pets.Contains(hit.collider.gameObject))
+                {
+                    //if we touching the same object or a different one
+                    if (curSelected != null && hit.collider.gameObject != curSelected)
+                        Select(hit.collider.gameObject);
+                    else if (curSelected == null)
+                        Select(hit.collider.gameObject);
+
+                }
+            }
+            else
+                Deselect();
+        }
+
+    }
+
+    void Select(GameObject selected)
+    {
+        if (curSelected != null)
+            ToggleSelectionVisual(curSelected, false);
+
+        curSelected = selected;
+        ToggleSelectionVisual(curSelected, true);
+        selectionUI.SetActive(true);
+
+    }
+
+    void Deselect()
+    {
+        if (curSelected != null)
+            ToggleSelectionVisual(curSelected, false);
+
+        curSelected = null;
+        //selectionUI.SetActive(false);
+    }
+
+    void ToggleSelectionVisual(GameObject obj, bool toggle)
+    {
+        obj.transform.Find("Selected").gameObject.SetActive(toggle);
+
+    }
+
     public void PlacePet(GameObject prefab)
     {
         GameObject obj = Instantiate(prefab, placement_Indicator.position, Quaternion.identity);
         pets.Add(obj);
-       
-        //select the object
+
+        Select(obj);
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+  
 }
