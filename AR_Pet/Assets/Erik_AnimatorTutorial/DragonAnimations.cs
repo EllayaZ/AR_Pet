c3ee88724a0156edb2ab4ffa8d53ac72b35076c2
Erik <erikroystan@gmail.com>
2020-05-27 17:42:54 -0700

    Add animator tutorial.

---
+++ AR_Pet/Assets/Erik_AnimatorTutorial/DragonAnimations.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DragonAnimations : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Animator>().SetBool("IsRunning", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("IsRunning", false);
+        }
+    }
+}
