fc6c7f2f03e85a39b144108b78f2c425d003f720
Ellaya Allyssandra Zampieri <61988562+EllayaZ@users.noreply.github.com>
2020-06-07 21:25:04 -0700

    Fixed and finale

---
+++ AR_Sara/Assets/Scripts/Animations.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    [SerializeField] private Camera arCamera;
+    private string animationState = "Running";
+
+    private void Update()
+    {
+        EnableTouchScreen();
+    }
+
+    /// <summary>
+    /// Enables the touch on the screen to switch between
+    /// Waiting and Running animation at any point.
+    /// </summary>
+    private void EnableTouchScreen()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject))
+                {
+                    if (hitObject.transform.CompareTag("Sara"))
+                    {
+                        if (animationState != "Waiting")
+                        {
+                            gameObject.GetComponent<Animator>().SetBool("Running", false);
+                            gameObject.GetComponent<Animator>().SetBool("Waiting", true);
+                            animationState = "Waiting";
+                        }
+                        else
+                        {
+                            gameObject.GetComponent<Animator>().SetBool("Running", true);
+                            gameObject.GetComponent<Animator>().SetBool("Waiting", false);
+                            animationState = "Running";
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Trigger animations from UI buttons.
+    /// </summary>
+    public void TriggerAnimation(string newAnimationState)
+    {
+        gameObject.GetComponent<Animator>().SetBool(animationState, false);
+        gameObject.GetComponent<Animator>().SetTrigger(newAnimationState);
+    }
+}
---
+++ AR_Sara/Assets/Scripts/Scene_Behavior.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Scene_Behavior : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToPlace = null;
+    [SerializeField] private Texture defaultTexture = null;
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+    private bool isObjectPlaced = false;
+
+    private void Awake()
+    {
+        // Getting the ARRaycastManager and Place Indicator sprite (as child):
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        // Hiding the Placement Indicator before start.
+        visual.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        if (!isObjectPlaced)
+        {
+            Place_Indicator();
+        }
+        
+        if (visual.activeSelf)
+        {
+            EnableTouchScreen();
+        }
+    }
+
+    private void Place_Indicator()
+    {
+        // Raycasting from the center of the screen to the nearest plane:
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // If the raycast hits a plane, activate the Placement Indicator:
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!visual.activeInHierarchy)
+            {
+                visual.SetActive(true);
+            }  
+        }
+    }
+
+    /// <summary>
+    /// If the screen is touched, place the object in the
+    /// Placement Indicator's position.
+    /// </summary>
+    private void EnableTouchScreen()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            objectToPlace.SetActive(true);
+            objectToPlace.transform.position = visual.transform.position;
+
+            visual.SetActive(false);
+            isObjectPlaced = true;
+        }
+    }
+
+    /// <summary>
+    /// Receives a float number from a UI interactive button
+    /// in order to rotate the object placed.
+    /// </summary>
+    public void Rotate(float degrees)
+    {
+        if (objectToPlace.activeSelf)
+        {
+            objectToPlace.transform.eulerAngles += Vector3.up * degrees;
+        }
+    }
+
+    /// <summary>
+    /// Receives a float number from a UI interactice button
+    /// to scale the object placed.
+    /// </summary>
+    public void Scale(float scaleRate)
+    {
+        if (objectToPlace.activeSelf)
+        {
+            objectToPlace.transform.localScale += Vector3.one * scaleRate;
+        }
+    }
+
+    /// <summary>
+    /// Changes the material of the object applying the new
+    /// received texture.
+    /// </summary>
+    public void SetColor(Texture newTexture)
+    {
+        objectToPlace.GetComponentInChildren<Renderer>().material.mainTexture = newTexture;
+    }
+
+    /// If the object is deleted, it gets into its default values
+    /// and gets disabled. Place Indicator Method restart in order
+    /// to place the object again in a different position.
+    /// </summary>
+    public void DeleteObject()
+    {
+        objectToPlace.transform.rotation = new Quaternion(0.0f, 180.0f, 0.0f, 0.0f);
+        objectToPlace.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        SetColor(defaultTexture);
+        objectToPlace.SetActive(false);
+        isObjectPlaced = false;
+    }
+}
